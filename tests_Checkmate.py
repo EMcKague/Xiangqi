551b7626a18042a6f7f846b9e156d97a8055aecd
EMcKague <evan.mckague@gmail.com>
2021-02-27 11:01:29 -0700

    Finished up game and formatting added and changed some tests more testing and doc string notes needed

---
+++ tests_Checkmate.py
@@ -0,0 +1,19 @@
+import unittest
+from XiangqiGame import XiangqiGame
+
+class TestCase(unittest.TestCase):
+    # TESTS  
+    def test1(self):
+        game = XiangqiGame()
+
+        # While game state == "UNFINISHED"
+        # choose a random red piece 
+            # of available moves, call make move for move 
+        # choose a random black piece 
+            # of available moves, call make move for move
+
+        self.assertEqual(location, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
